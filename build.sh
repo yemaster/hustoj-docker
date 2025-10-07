eef190d046b3988ba788006ccdd76cee6f0008f2
Yemaster <30290923+yemaster@users.noreply.github.com>
2025-10-07 19:22:44 +0800

    Update build script to use GITHUB_PROXY and add Docker builds
    Replaces the commented git clone with a command using the GITHUB_PROXY variable for cloning the hustoj repository. Adds Docker build steps for both web and judger components, tagging the resulting images appropriately.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 rm -rf ./web/src/
 rm -rf ./judger/src/
 
-# git clone https://github.com/zhblue/hustoj
+git clone ${GITHUB_PROXY}https://github.com/zhblue/hustoj
 
 mkdir ./web/src/
 mkdir ./judger/src/
@@ -13,5 +13,8 @@ cp -r ./hustoj/trunk/web/* ./web/src/
 cp -r ./hustoj/trunk/core/* ./judger/src/
 cp -r ./hustoj/trunk/install/java0.policy ./judger/etc/
 cp -r ./hustoj/trunk/install/judge.conf ./judger/etc/
+
+cd ./web && docker build -t yemaster/hustoj-web . && cd ..
+cd ./judger && docker build -t yemaster/hustoj-judger . && cd ..
 
 echo "Done."
